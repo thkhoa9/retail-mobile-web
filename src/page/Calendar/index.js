d5fa259793a9e1e97b56cc55396175eb2fe3cec6
thkhoa9 <nguyenthanhkhoa8888@gmail.com>
2024-11-22 16:45:18 +0700

    Xong sườn mobile

--- src/page/Calendar/index.js
+++ src/page/Calendar/index.js
@@ -55,15 +55,12 @@ export default function Calendar() {
             Lịch làm việc
           </h2>
         </div>
-        <div class="w-full overflow-x-auto">
-          <header class="flex items-center justify-between border-b border-gray-200 px-6 py-4 lg:flex-none">
-            <h1 class="text-base font-semibold leading-6 text-gray-900">
-              {/* <time datetime="2022-01">January 2022</time> */}
-            </h1>
+        <div class="w-auto ">
+          <header class=" w-full flex items-center justify-start border-b border-gray-200  py-4 flex-none">
             <div class="w-full flex items-center justify-start">
               <button
                 type="button"
-                className="btn w-10 ml-14 justify-items-center border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-white "
+                className="btn w-28 justify-items-center border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-white "
                 onClick={handlePrev}
               >
                 <svg
@@ -78,14 +75,14 @@ export default function Calendar() {
                     clip-rule="evenodd"
                   />
                 </svg>
-                
+                Trước 
               </button>
               <button
                 type="button"
-                className="btn w-10 ml-1 justify-items-center  border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-white "
+                className="btn w-28 ml-1 justify-items-center  border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-white "
                 onClick={handleNext}
               >
-              
+              Sau
                 <svg
                   class="h-5 w-5"
                   viewBox="0 0 20 20"
@@ -98,28 +95,29 @@ export default function Calendar() {
                     clip-rule="evenodd"
                   />
                 </svg>
+                
               </button>
               <input
                     type="date"
-                    className="input w-56 ml-2 border-2  rounded-md"
+                    className="input w-40 ml-2 border-2  rounded-md"
                     onChange={handleDateChange}
                   />
             </div>
           </header>
 
-          <div class="shadow ring-1 ring-black ring-opacity-5 ">
+          <div class="w-auto shadow ring-1 ring-black ring-opacity-5 overflow-x-auto">
             <div class="grid grid-cols-7 w-full h-24  gap-px border-b border-gray-300 bg-gray-200 text-center text-xs font-semibold leading-6 text-gray-700 ">
               {daysOfWeek.map((day, index) => (
-                <div class="justify-center bg-white py-2 ">
-                  <div class="sr-only sm:not-sr-only">{dayNames[index]}</div>
-                  <h1 class="sr-only sm:not-sr-only">
+                <div class="justify-center bg-white py-2">
+                  <div class="sr-only not-sr-only whitespace-nowrap">{dayNames[index]}</div>
+                  <h1 class="sr-only not-sr-only whitespace-nowrap">
                     {day.format("DD/MM/YYYY")}
                   </h1>
                 </div>
               ))}
             </div>
             <div class="flex bg-gray-200 text-xs leading-6 text-gray-700 ">
-              <div class="hidden w-full lg:grid lg:grid-cols-7 lg:grid-rows-6 lg:gap-px">
+              <div class=" w-full grid grid-cols-7 grid-rows-6 gap-px">
                 {/*
                     Always include: "relative py-2 px-3"
                     Is current month, include: "bg-white"
@@ -128,28 +126,25 @@ export default function Calendar() {
                 {daysOfWeek.map((day, index) => (
                   <div
                     key={index}
-                    class="w-40 relative bg-white px-3 py-2 text-gray-500 "
-                    style={{
-                      height: "calc(100vh - 20px)",
-                    }}
+                    class="relative bg-white px-3 py-2 text-gray-500 "
                   >
                     {/*
                         Is today, include: "flex h-6 w-6 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white"
                     */}
-                    <time dateTime={day.format("YYYY-MM-DD")}>
+                    <time dateTime={day.format("YYYY-MM-DD")} className="whitespace-nowrap">
                       7:30 - 12:30{" "}
                     </time>
                   </div>
                 ))}
                 {daysOfWeek.map((day, index) => (
                   <div key={index} class="relative bg-white px-3 py-2">
-                    <time dateTime={day.format("YYYY-MM-DD")}>
+                    <time dateTime={day.format("YYYY-MM-DD")} className="whitespace-nowrap">
                       12:30 - 17:30
                     </time>
                     <ol class="mt-2">
                       <li>
                         <a href="" class="group flex">
-                          <p class="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600">
+                          <p class="flex-auto truncate font-medium text-gray-900 group-hover:text-indigo-600 whitespace-nowrap">
                             Nguyễn Thanh Khoa
                           </p>
                         </a>
@@ -158,7 +153,7 @@ export default function Calendar() {
                   </div>
                 ))}
                 {daysOfWeek.map((day, index) => (
-                  <div key={index} class="relative bg-white px-3 py-2">
+                  <div key={index} class="relative bg-white px-3 py-2 whitespace-nowrap">
                     <time dateTime={day.format("YYYY-MM-DD")}>
                       17:30 - 22:30
                     </time>
